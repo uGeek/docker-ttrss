38fc80f338151fa04ae34d9212c4b98d549aa781
Robert Kaussow <mail@geeklabor.de>
2019-07-17 12:17:40 +0200

    add healthcheck script

---
+++ overlay/usr/local/bin/healthcheck.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eo pipefail
+URL=http://localhost
+
+wget --quiet --tries=1 --spider ${URL}
+[ $? -ne 0 ] && exit 1
+
+CONTENT=$(wget --quiet -O - ${URL})
+case "$CONTENT" in
+  *Exception*) exit 1 ;;
+esac
+
+exit 0
