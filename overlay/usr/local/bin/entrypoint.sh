0f5296d0272378dd9920adb9d5558a270a44b979
Robert Kaussow <mail@geeklabor.de>
2019-07-20 14:34:59 +0200

    small fixes in healthcheck and entrypoint script

--- overlay/usr/local/bin/entrypoint.sh
+++ overlay/usr/local/bin/entrypoint.sh
@@ -2,6 +2,11 @@
 /usr/local/bin/gomplate -V -o /etc/php7/php.ini -f /etc/templates/php.ini.tmpl
 /usr/local/bin/gomplate -V -o /var/www/app/config.php -f /etc/templates/config.php.tmpl
 
+# ensure lock folders exists
+mkdir -p /var/www/app/lock/images
+mkdir -p /var/www/app/lock/upload
+mkdir -p /var/www/app/lockexport
+
 chown -R nginx:nginx /var/www/app/lock
 chown -R nginx:nginx /var/www/app/cache
 chown -R nginx:nginx /var/www/app/feed-icons
--- overlay/usr/local/bin/healthcheck.sh
+++ overlay/usr/local/bin/healthcheck.sh
@@ -9,6 +9,7 @@ wget --quiet --tries=1 --spider ${URL}
 CONTENT=$(wget --quiet -O - ${URL})
 case "$CONTENT" in
   *Exception*) exit 1 ;;
+  *misconfiguration*) exit 1 ;;
 esac
 
 exit 0
