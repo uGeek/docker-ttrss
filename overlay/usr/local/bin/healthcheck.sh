0471467e64e54341fe523019c9d099301d086f97
Robert Kaussow <mail@geeklabor.de>
2019-07-20 16:19:23 +0200

    fix healthcheck and add cache dir setup

--- overlay/usr/local/bin/healthcheck.sh
+++ overlay/usr/local/bin/healthcheck.sh
@@ -9,6 +9,7 @@ wget --quiet --tries=1 --spider ${URL}
 CONTENT=$(wget --quiet -O - ${URL})
 case "$CONTENT" in
   *Exception*) exit 1 ;;
+  *alert-*alert-*SELF_URL_PATH*) exit 1 ;;
   *alert-*SELF_URL_PATH*alert-*) exit 1 ;;
   *SELF_URL_PATH*alert-*alert-*) exit 1 ;;
 esac
